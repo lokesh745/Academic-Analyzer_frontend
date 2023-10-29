1d4c48022714ad35f504efb4f7d33bbfe2de9a20
Sarvesh <sarvesh.damle1@gmail.com>
2023-10-30 01:19:48 +0530

    Some changes

--- app/components/Navbar/Navbar.tsx
+++ app/components/Navbar/Navbar.tsx
@@ -1,4 +1,3 @@
-"use client";
 import React, { useState } from "react";
 import { RxHamburgerMenu, RxCross1 } from "react-icons/rx";
 import { PiUserCircle } from "react-icons/pi";
@@ -12,14 +11,14 @@ export default function Navbar({}: Props) {
     <div className="border-b-2 fixed w-full z-10  border-gray-700">
       <nav className="text-white h-[70px] bg-[#212529] flex justify-between backdrop-filter backdrop-blur-sm   ">
         <div className="flex lg:gap-x-28">
-          <div className="w-[70px] flex items-center hover:cursor-pointer mx-5  justify-ceter">
+          <div className="w-[70px] flex items-center hover:cursor-pointer mx-5  justify-center">
             VIT
           </div>
           <div className="lg:flex w-[700px] lg:justify-between lg:items-center hidden   ">
-            <div className="hover:cursor-pointer">Dashboard</div>
-            <div className="hover:cursor-pointer">User</div>
-            <div className="hover:cursor-pointer">Courses</div>
-            <div className="hover:cursor-pointer">Department</div>
+            <div className="hover:cursor-pointer hover:text-blue-400">Dashboard</div>
+            <div className="hover:cursor-pointer hover:text-blue-400">User</div>
+            <div className="hover:cursor-pointer hover:text-blue-400">Courses</div>
+            <div className="hover:cursor-pointer hover:text-blue-400">Department</div>
           </div>
         </div>
         <div className="w-[130px]  lg:w-[130px]  flex items-center lg:justify-around hover:cursor-pointer justify-around mx-5 ">
@@ -27,11 +26,11 @@ export default function Navbar({}: Props) {
             <PiUserCircle size={28} />
           </div>
           <Link href="/">
-            <div className=" hidden lg:flex  ">Logout</div>
+            <div className=" hidden lg:flex  hover:text-blue-400">Logout</div>
           </Link>
           {hamburgerState ? (
             <div
-              className="lg:hidden ml-3 "
+              className="lg:hidden ml-3 hover:text-blue-400"
               onClick={() => setHamburgerState(!hamburgerState)}
             >
               <RxHamburgerMenu size={28} />
@@ -53,23 +52,23 @@ export default function Navbar({}: Props) {
             : "bg-[#212529] w-full  flex flex-col  lg:hidden absolute top-[70px]"
         }
       >
-        <div className="hover:cursor-pointer border  border-gray-700 py-3 w-full flex justify-center text-white">
+        <div className="hover:cursor-pointer hover:text-blue-400 border  border-gray-700 py-3 w-full flex justify-center text-white">
           Dashboard
         </div>
-        <div className="hover:cursor-pointer border  border-gray-700 py-3 w-full flex justify-center text-white">
+        <div className="hover:cursor-pointer hover:text-blue-400 border  border-gray-700 py-3 w-full flex justify-center text-white">
           Employee
         </div>
-        <div className="hover:cursor-pointer border  border-gray-700 py-3 w-full flex justify-center text-white">
+        <div className="hover:cursor-pointer hover:text-blue-400 border  border-gray-700 py-3 w-full flex justify-center text-white">
           Jobs
         </div>
-        <div className="hover:cursor-pointer border  border-gray-700 py-3 w-full flex justify-center text-white">
+        <div className="hover:cursor-pointer hover:text-blue-400 border  border-gray-700 py-3 w-full flex justify-center text-white">
           Candidates
         </div>
-        <div className="hover:cursor-pointer border  border-gray-700 py-3 w-full flex justify-center text-white">
+        <div className="hover:cursor-pointer hover:text-blue-400 border  border-gray-700 py-3 w-full flex justify-center text-white">
           Leaves
         </div>
         <Link href="/">
-          <div className="hover:cursor-pointer border  border-gray-700 py-3 w-full flex justify-center text-white">
+          <div className="hover:cursor-pointer hover:text-blue-400 border  border-gray-700 py-3 w-full flex justify-center text-white">
             Logout
           </div>
         </Link>
